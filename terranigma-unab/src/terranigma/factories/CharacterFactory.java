d78fd4cac94a09b07fd9e3bb048bd15e2c588fba
Gaston Marsano <gstnmarsano@gmail.com>
2019-08-15 21:50:37 -0400

    CharacterFactory, personajes necesarios y esqueletos.

---
+++ terranigma-unab/src/terranigma/factories/CharacterFactory.java
@@ -0,0 +1,41 @@
+package terranigma.factories;
+
+import terranigma.abstracts.Character;
+import terranigma.characters.*;
+
+public class CharacterFactory {
+	private static final CharacterFactory singleton = new CharacterFactory();
+	private final String[] characterList = {
+		"Ark",
+		"Naomi",
+		"El Sabio"
+	};
+	
+	public static CharacterFactory get() {
+		return singleton;
+	}
+	
+	private CharacterFactory() {}
+	
+	public String[] getCharacterList() {
+		return this.characterList;
+	}
+	
+	public Character newCharacter(String type) {
+		Character response;
+		switch(type) {
+			case "Ark":
+				response = new Ark();
+				break;
+			case "Naomi":
+				response = new Naomi();
+				break;
+			case "El Sabio":
+				response = new ElSabio();
+				break;
+			default:
+				response = new Ark();
+		}
+		return response;
+	}
+}
